97206f7ac8de8167e7238df81329beaefdd05655
Chaitanya-Sarma-hub <iamsarma030@gmail.com>
2022-09-07 16:30:44 +0530

    added functionality for login button in navbar

--- app/src/main/java/com/chaitanya/smack/MainActivity.kt
+++ app/src/main/java/com/chaitanya/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.chaitanya.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.navigation.NavigationView
@@ -45,7 +46,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginButtonNavClicked(view : View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View) {
@@ -55,4 +57,5 @@ class MainActivity : AppCompatActivity() {
     fun sendMessageButtonClicked(view: View) {
 
     }
+
 }
